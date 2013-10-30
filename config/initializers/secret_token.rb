dd70b2d46c97406303aa7c3e02f99f0e1de8d4da
meshin <majormeshin@gmail.com>
2013-10-30 16:47:10 +0100

    Little spajder on the Wall

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spajder::Application.config.secret_token = '60c278a6b56a382184d2d9edbbbe78dd1412e48113285ea9cc88b73cdc39692f78caec203acf756842027c1afbce460d76e8bd4450b83ceb61de6d976677c865'
